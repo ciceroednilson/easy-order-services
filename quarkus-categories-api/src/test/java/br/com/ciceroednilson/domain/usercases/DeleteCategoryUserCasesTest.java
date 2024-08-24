943788ffb723988d48987fc484ebd160a731c2eb
cicero <ciceroednilson@gmail.com>
2024-08-24 20:35:11 -0300

    Adding the unit test

---
+++ quarkus-categories-api/src/test/java/br/com/ciceroednilson/domain/usercases/DeleteCategoryUserCasesTest.java
@@ -0,0 +1,37 @@
+package br.com.ciceroednilson.domain.usercases;
+
+import br.com.ciceroednilson.domain.repository.CategoryRepository;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.jboss.logging.Logger;
+
+import static org.mockito.Mockito.*;
+
+@QuarkusTest
+public class DeleteCategoryUserCasesTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private DeleteCategoryUserCases deleteCategoryUserCases;
+
+    @Mock
+    private Logger logger;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testDelete() {
+        final long id = 1L;
+        deleteCategoryUserCases.delete(id);
+        verify(categoryRepository, times(1)).delete(id);
+    }
+}
