d4976b041c6e15b5fe2af2e6edf7e42be7b77ac5
cicero <ciceroednilson@gmail.com>
2024-08-25 16:23:36 -0300

    update same script of mysql

--- mysql-scripts-database/01-create-table-category.sql
+++ mysql-scripts-database/01-create-table-category.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tb_category (
-    id_category INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_category BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ds_category VARCHAR(150) NOT NULL,
     dt_created DATETIME NOT NULL,
     fl_active BOOLEAN NOT NULL
--- mysql-scripts-database/02-create-table-product.sql
+++ mysql-scripts-database/02-create-table-product.sql
@@ -1,11 +1,12 @@
+
 CREATE TABLE IF NOT EXISTS tb_product (
-	id_product  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	ds_product  VARCHAR(150) NOT NULL,
-	id_category INT NOT NULL,
-	vl_product  DECIMAL(10,2) NOT NULL,
-	qt_product  INT NOT NULL,
-	dt_created  DATETIME NOT NULL,
-	dt_updated  DATETIME NOT NULL,
-	fl_active   BOOLEAN NOT NULL,
+    id_product  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ds_product  VARCHAR(150) NOT NULL,
+    id_category BIGINT NOT NULL,
+    vl_product  DECIMAL(10,2) NOT NULL,
+    qt_product  INT NOT NULL,
+    dt_created  DATETIME NOT NULL,
+    dt_updated  DATETIME NOT NULL,
+    fl_active   BOOLEAN NOT NULL,
     FOREIGN KEY (id_category) REFERENCES tb_category(id_category)
 );
